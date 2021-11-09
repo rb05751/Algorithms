582b0a639c8da2d11362bb00c7378e9694b7b2df
ryanbeasley <rdbeasley1995@gmail.com>
2021-11-09 10:20:41 -0500

    adding quick_sort and did comparable testing

---
+++ sorting/quick_sort.py
@@ -0,0 +1,60 @@
+import random
+import copy
+
+
+class QuickSort:
+    def __init__(self, items, is_random=False):
+        self.items = items
+        self.n = len(items)
+        self.is_random = is_random
+
+    def check(self, A):
+        if len(A) != 0:
+            last = A[0]
+            for i in range(1,len(A)):
+                if A[i] != last:
+                    return True
+                else:
+                    continue
+            return False
+        else:
+            return False
+
+    def __swap(self, A, idx1, idx2):
+        temp = A[idx1]
+        A[idx1] = A[idx2]
+        A[idx2] = temp
+        return A
+
+    def reg_partition(self, A):
+        """Takes in an array or list (A) and returns Q and index to partition the list/array on"""
+        if self.is_random:
+            idx = random.randint(0, len(A) - 1)
+        else:
+            idx = -1
+        pivot = A[idx]
+        self.__swap(A, idx1=idx, idx2=-1)
+        i = -1
+        for j in range(len(A) - 1):
+            if A[j] <= pivot:
+                i += 1
+                A = self.__swap(A, idx1=j, idx2=i)
+
+        A = self.__swap(A, idx1=i + 1, idx2=-1)
+        return A, i + 1
+
+    def sort(self, A):
+        if len(A) <= 1:
+            return A
+        else:
+            A, q = self.reg_partition(A)
+            return self.sort(A[:q]) + [A[q]] + self.sort(A[q + 1:])
+
+
+if __name__ == '__main__':
+    num_list = [5, 2, 4, 6, 1, 3, 3, 8, 9, 11, 7, 0, 1, 4, 10, 12]
+    sorter = QuickSort(items=num_list, is_random=False)
+    if sorter.check(A=num_list):
+        print(sorter.sort(num_list))
+    else:
+        print(num_list)
--- test.py
+++ test.py
@@ -1,22 +1,49 @@
 import random
 import sys
-
-def RandSearch(arr, x, checked):
-    if len(checked) == len(arr):
-        print(checked)
-        return False
-    else:
-        i = random.randint(0, len(arr)-1)
-        if arr[i] == x:
-            print(arr[i])
-            return True
-        else:
-            if arr[i] not in checked:
-                checked.append(arr[i])
-            return RandSearch(arr, x, checked)
+from sorting import bubble_sort, heap_sort, insertion_sort, merge_sort, quick_sort
+import time
 
 if __name__ == '__main__':
-    sys.setrecursionlimit(10**5)
-    value = 7
-    items = [3,2,1,5,6,9,8]
-    print(RandSearch(arr=items, x=value, checked=[]))
+    num_list = [5, 2, 4, 6, 1, 3, 3, 8, 9, 11, 7, 0, 1, 4, 10, 12]
+    num_list = list(range(0, 1000))
+    random.shuffle(num_list)
+    quick_sorter = quick_sort.QuickSort(items=num_list, is_random=False)
+    merge_sorter = merge_sort.MergeSort(items=num_list)
+    insertion_sorter = insertion_sort.InsertionSort(items=num_list, ascending=True)
+    heap_sorter = heap_sort.HeapSort(items=num_list)
+    bubble_sorter = bubble_sort.BubbleSort(items=num_list)
+    if quick_sorter.check(A=num_list):
+        """Test"""
+
+        """QuickSort"""
+        start = time.time()
+        q_result = quick_sorter.sort(A=num_list)
+        print(f"QuickSort: {q_result}...")
+        print(f"Time: {time.time() - start} seconds")
+
+        """MergeSort"""
+        start = time.time()
+        m_result = merge_sorter.run()
+        print(f"MergeSort: {m_result}...")
+        print(f"Time: {time.time() - start} seconds")
+
+        """Insertion Sort"""
+        start = time.time()
+        i_result = insertion_sorter.sort()
+        print(f"InsertionSort: {i_result}...")
+        print(f"Time: {time.time() - start} seconds")
+
+        """Heap Sort"""
+        start = time.time()
+        h_result = heap_sorter.sort()
+        print(f"Heap Sort: {h_result}...")
+        print(f"Time: {time.time() - start} seconds")
+
+        """Bubble Sort"""
+        start = time.time()
+        b_result = bubble_sorter.run()
+        print(f"Bubble Sort: {b_result}...")
+        print(f"Time: {time.time() - start} seconds")
+
+    else:
+        print(num_list)
