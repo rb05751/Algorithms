c2bc67b68ee9ad4adedda3ffd53384363e8a757f
Ryan <ryanbeasley@benzinga.com>
2022-01-03 22:15:38 -0500

    adding SELECT algorithm to repo

--- search_and_select/select.py
+++ search_and_select/select.py
@@ -8,8 +8,9 @@ Array = List[float]
 
 
 class Select:
-    def __init__(self, items: Array):
+    def __init__(self, items: Array, order: int):
         self.items = items
+        self.ith = order
 
     def __divide(self) -> List[float]:
         """divides items into groups of floor(n/5)"""
@@ -34,15 +35,45 @@ class Select:
         else:
             sorter = InsertionSort(items=items, ascending=True)
             sorted_items = sorter.sort()
-            return sorted_items[len(sorted_items) // 2]  if len(sorted_items) > 2 else sorted_items[0]
+            if len(sorted_items) == 3:
+                idx = sorted_items[len(sorted_items) // 2]
+            else:
+                idx = sorted_items[len(sorted_items) // 2-1]
+            return idx if len(sorted_items) > 2 else sorted_items[0]
 
+    def __swap(self, A, idx1, idx2):
+        temp = A[idx1]
+        A[idx1] = A[idx2]
+        A[idx2] = temp
+        return A
 
+    def __partition(self, items, q):
+        if len(items) <= 1:
+            return items, 0
+        items = self.__swap(items, idx1=items.index(q), idx2=-1) # swap with last item in Array
+        i = -1
+        for j in range(len(items)):
+            if items[j] <= q:
+                i += 1
+                items = self.__swap(A=items, idx1=i, idx2=j)
 
-    def compute(self):
-        return self.__find_median(items=self.items)
+        ## swap q back to rightful position
+        items = self.__swap(A=items, idx1=i+1, idx2=-1)
+        return items, i
+
+    def select(self, items, k):
+        median = self.__find_median(items)
+        new_items, partition_idx = self.__partition(items=items, q=median)
+        if partition_idx == k-1:
+            return new_items[partition_idx]
+        elif partition_idx > k-1:
+            return self.select(items=new_items[:partition_idx], k=k)
+        else:
+            return self.select(items=new_items[partition_idx+1:], k=k-partition_idx-1)
 
 if __name__ == '__main__':
-    import statistics
     items = [3, 4, 7, 1, 5, 8, 9]
-    select = Select(items=items)
-    print(select.compute())
+    order = 9
+    select = Select(items=items, order=order)
+    print(select.select(items, k=order))
+    print(list(sorted(items))[order-1])
